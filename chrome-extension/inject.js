d63840794072c4a28e7bf4de33f996be2b875d5c
Ross Simpson <ross.simpson@gmail.com>
2021-03-22 14:27:08 +0200

    Added comments

--- chrome-extension/inject.js
+++ chrome-extension/inject.js
@@ -1,6 +1,8 @@
 (function() {
+  // find all of the search results
   var results = document.getElementsByClassName("small-3");
 
+  // sort them by number of reviews
   var array = [];
   for (var i=0; i<results.length; i++) {
     elem = results[i];
@@ -9,13 +11,14 @@
       reviews: elem.getElementsByClassName("rating-module_review-count_3g6zO")[0] ? parseInt(elem.getElementsByClassName("rating-module_review-count_3g6zO")[0].innerText.replace(' (', '').replace(')', '')) : 0
     });
   }
-
   var sorted = array.sort(function(a, b) {
     return (a.reviews > b.reviews) ? 1 : ((b.reviews > a.reviews) ? -1 : 0)
   }).reverse();
 
+  // find the partent container
   var parent = sorted[0].elem.parentElement;
 
+  // remove all of the results
   for (var i=0; i<sorted.length; i++) {
     if (parent) {
       parent.removeChild(sorted[i].elem)
@@ -27,6 +30,7 @@
     parent.removeChild(parent.lastChild);
   }
 
+  // add the results back
   for (var i=0; i<sorted.length; i++) {
     if (parent) {
       parent.appendChild(sorted[i].elem);
